ca622d6c2c7dee37d5de4127098fc627478a3014
Birunthaban Sarventhiran <birunth-se19006@stu.kln.ac.lk>
2023-03-07 11:15:52 +0530

    initialize backend ,create database , initial commit

---
+++ src/main/java/com/ecommerce/backend/model/Customer.java
@@ -0,0 +1,64 @@
+package com.ecommerce.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String userName;
+
+    private String email;
+    private String password ;
+    private String phoneNumber ;
+    private Boolean status ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
