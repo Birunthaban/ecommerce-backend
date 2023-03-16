e1e5ca197c036b5d26cc4ae30d81a85fc020c4c1
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-03-16 16:06:43 +0530

    created the cart table cord and id

--- src/main/java/com/ecommerce/backend/model/Cart.java
+++ src/main/java/com/ecommerce/backend/model/Cart.java
@@ -1,4 +1,16 @@
 package com.ecommerce.backend.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Cart")
 public class Cart {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int Id;
+
+
+    
 }
+
