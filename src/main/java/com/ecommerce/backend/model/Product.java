9fcf152f6c0c10867db548873c9c137e88a7a228
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-03-17 08:20:55 +0530

    Update column

--- src/main/java/com/ecommerce/backend/model/Product.java
+++ src/main/java/com/ecommerce/backend/model/Product.java
@@ -16,12 +16,13 @@ public class Product {
     private String productName;
     @Column(name="Product_Qty",nullable = false)
     private int productQut;
-    @Column(name="Product_Price")
+    @Column(name="Product_Price",nullable = false)
     private float productPrice;
-    @Column(name="Product_Description")
-    private String ProductDescription;
-    @Column(name="made_In")
+    @Column(name="Product_Description",nullable = false)
+    private String productDescription;
+    @Column(name="Made_In",nullable = false)
     private String madeInCountry;
-
+    @Column(name="Product_Category",nullable = false)
+    private  String productCategory;
 
 }
